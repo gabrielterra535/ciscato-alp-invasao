f3691f4789ab48a4b423abba607f99bbdb9f4282
Gabriel Terra <gabrielterra535@gmail.com>
2019-10-29 16:27:48 -0300

    início do código

--- resolucao.py
+++ resolucao.py
@@ -1,2 +1,17 @@
 # Teste
-print("Ola mundo")
+a = [3, 4, 50, 2, 13, 67, 4 , 23, 18]
+b = [5, 6, 22, 122, 34, 67, 89, 32, 189,25 ,53 ,67 ,125]
+c = [5, 6, 22, 123, 34, 67, 89, 32, 189, 25 ,53 ,67 ,125]
+
+print('''digite um código para obter a data: 
+	a = [3, 4, ...], 
+	b = [5, 6, 22,122, ...] ,
+	c = [5,6, 22, 123, ...]  ''')
+
+x = input(' digite a variável correspondente ao código: ')
+
+if 	x == 'a' :
+
+elif x == 'b' :
+
+else:  
